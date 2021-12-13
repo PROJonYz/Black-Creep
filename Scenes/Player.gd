0aca7622a79915c86f13c46db372af6387032dc0
PROJonYz <67200060+PROJonYz@users.noreply.github.com>
2021-12-13 13:15:40 +0200

    v0.1.0

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,20 +1,42 @@
 extends KinematicBody2D
 
+var vel = Vector2()
+var gravity = 900
+var jumpforce = -190
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	pass
+	
+func _physics_process(delta):
+	vel.x = 0
+	
+	if Input.is_action_pressed("Left"):
+		vel.x -= 60
+		$Sprite.flip_h = true
+		$Sprite.animation = "Walk"
+	elif Input.is_action_pressed("Right"):
+		vel.x += 60
+		$Sprite.flip_h = false
+		$Sprite.animation = "Walk"
+	else:
+		vel.x = 0
+		$Sprite.animation = "Idle"
+		
+	if Input.is_action_pressed("Jump") and is_on_floor():
+		vel.y += jumpforce
+		$JumpSFX.play()
+		
+	if Input.is_action_pressed("Esc"):
+		get_tree().change_scene("res://Scenes/Levels/MainMenu.tscn")
+		
+	vel.y += delta * gravity
+	
+	vel = move_and_slide(vel, Vector2.UP)
 
 func _on_Hitbox_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("Key"):
+		$KeySFX.play()
+		$Switch.start()
+	
+func _on_Switch_timeout():
+	Global.key_is_picked = true
